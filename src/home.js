126e69ec03b80648bf82f238d4127b529db94a12
Vadym <vadim.drinevskiy@gmail.com>
2025-04-12 11:39:10 +0300

    api

--- src/home.js
+++ src/home.js
@@ -1,1 +1,13 @@
 //Логіка сторінки Home
+
+import { getCategories, getProducts, getProductsByCategory, getProductsById, getSearchProduct } from "./js/products-api";
+
+// console.log(await getCategories());
+
+console.log(await getProducts(1));
+
+// console.log(await getProductsById(1));
+
+// console.log(await getSearchProduct("phone"));
+
+// console.log(await getProductsByCategory("smartphones"));
